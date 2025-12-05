5dd2a429b71fc1a76ac56ec3dfc7b6cf099fc266
Harsh Shah <harshshah6002@gmail.com>
2025-12-05 15:47:54 +0530

    sorting

--- sorting.cpp/bubbleSort.cpp
+++ sorting.cpp/bubbleSort.cpp
@@ -2,20 +2,38 @@
 using namespace std;
 
 int main() {
-    int array[10]={1,3,4,6,2,7,6,9,8,10};
+    int size;
     
-    for(int i=0;i<10;i++){
-        for(int j=0;j<10;j++){
-            if(array[j]>array[j+1])
-            { int temp = array[j+1];
-            array[j+1]=array[j];
-            array[j]=temp;
+    cout << "Enter number of elements: ";
+    cin >> size;
+
+    if(size <= 0) {
+        cout << "Size must be positive!\n";
+        return 0;
+    }
+
+    int array[1000];
+
+    cout << "Enter " << size << " elements:\n";
+    for(int i = 0; i < size; i++) {
+        cin >> array[i];
+    }
+
+    
+    for(int i = 0; i < size; i++) {
+        for(int j = 0; j < size-1; j++) {   
+            if(array[j] > array[j+1]) {
+                int temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
             }
         }
     }
-    
-    for(int i=0;i<10;i++)
-    printf("%d\n", array[i]);
-    
+
+    cout << "\nSorted array:\n";
+    for(int i = 0; i < size; i++) {
+        cout << array[i] << "\n";
+    }
+
     return 0;
 }
